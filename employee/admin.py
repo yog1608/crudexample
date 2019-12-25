2fc00e4aa4fc9d01ffe187e28e5c5ce69ca849b8
yog1608 <rathodyogesh1608@gmail.com>
2019-12-25 17:44:12 +0530

    Initial commit

---
+++ employee/admin.py
@@ -0,0 +1,6 @@
+# from django.contrib import admin
+
+# Register your models here.
+from django.contrib import admin
+from employee.models import Employee
+admin.site.register(Employee) # Employee is registered
